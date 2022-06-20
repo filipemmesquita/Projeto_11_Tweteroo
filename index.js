bf001392ad47a03be0dec6741ef1f46b1c9dee81
Filipe Mesquita <filipemmesquita95@gmail.com>
2022-06-20 12:09:33 -0300

    Changes to be committed: 	new file:   .wakatime-project 	modified:   index.js

--- index.js
+++ index.js
@@ -5,13 +5,31 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const users=[]
+const tweets=[]
+
 app.post('/sign-up', (req, res) => {
-  
-  console.log(req.headers)
-  console.log(req.body)
+  users.push(req.body)
+  res.send("OK");
   });
-  
 
+app.post('/tweets', (req, res) => {
+  tweets.push(req.body)
+  res.send("OK");
+});
 
+app.get('/tweets', (req, res) => {
+  const recentTweets=tweets.slice(tweets.length-10);
+  const responseTweets=[];
+  recentTweets.forEach((tweet,index) => {
+    responseTweets[index]= { 
+      username: tweet.username,
+      tweet: tweet.tweet,
+      avatar: users.find(user=>user.username===tweet.username).avatar
+    }
+  })
+  res.send(responseTweets)
+});
 
+//
 app.listen(5000);
